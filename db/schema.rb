1b3be6998dc4252959d0c8ee22e4c7a07b013eb1
Cory Spitzer & Paul Ogawa <student@epicodus.com>
2015-06-29 12:28:32 -0700

    Add task logic and tests, associated with lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629162637) do
+ActiveRecord::Schema.define(version: 20150629191306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "list_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe List do
   it { should validate_presence_of :title}
+
 end
